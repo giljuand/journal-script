ea5468ccec2a0bfbc163b5b699ca77729f2c13e8
Juan Gil <35203589+giljuand@users.noreply.github.com>
2021-03-29 14:52:36 -0400

    Add example journal script

---
+++ journal (copy).sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NEW_FILE=/path/to/new_file   #temporarily stores latest entry
+MAIN_FILE=/path/to/main_file #stores all entries
+
+#checks if newest-entry.md exists
+if [ -f "$NEW_FILE" ]; then
+	echo -e "\n\n" >> "$MAIN_FILE"    #adds a line between entries
+	cat "$NEW_FILE" >> "$MAIN_FILE"   #appends new entry
+
+	#deletes newest-entry.md
+	rm "$NEW_FILE"
+fi
+
+#creates new file
+touch "$NEW_FILE"
+
+#adds the date as the heading
+echo -e "#$(date +"%A, %b %d, %Y %H:%M:%S")" >> "$NEW_FILE"
+
+#opens the file
+remarkable "$NEW_FILE"
+
